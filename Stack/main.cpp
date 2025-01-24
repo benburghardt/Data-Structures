576df4d2be5df8873e0017e2c5563955c40c8d33
RadActPro <86620400+RadActPro@users.noreply.github.com>
2025-01-23 23:56:36 -0700

    wrote simple time tests in main

--- Stack/main.cpp
+++ Stack/main.cpp
@@ -1,90 +1,48 @@
+/*
+ * Author: Ben Burghardt
+ */
+
 #include <chrono>
-//#include <iostream>
-#include <fstream>
-#include <random>
-#include <time.h>
-#include <vector>
-//#include "Node.h"
-// #include "stack-optimized.h"
+#include <iostream>
+#include <iomanip>
+
 #include "Naive-Stack.hpp"
 #include "Improved-Stack.hpp"
-
-
 using namespace std;
 
-//double time_n_pushes(unsigned n);
-double time_n_ksorts(vector<int>, int k);
+// Function prototypes
+double time_n_pushes_improved(unsigned n);
+double time_n_pushes_naive(unsigned n);
+
+double time_n_pops_improved(unsigned n);
+double time_n_pops_naive(unsigned n);
+
+double time_multi_improved(unsigned n);
+double time_multi_naive(unsigned n);
 
 int main (){
-    random_device rd;
-    mt19937 mt(rd());
+    unsigned iterations = 50000;
 
-    srand(time(0));
-    rand();
-    ofstream data("sortData.csv");
-    data << "k,n,time\n";
-    vector<int>* list;
-    int ks[] = {2,3,5,10,50,100,200,500};
-    int sizes[] = {100000,200000,300000,400000,500000,700000,1000000};
-    //bool con(true);
-    //while (con){
-        /*cout << "How many elements do you want to add: ";
-        cin >> size;
-        cout << endl;*/
-        for(int i = 0; i < 8; i++){
-            for(int j = 0; j < 7; j++){
-                
-                //double count = 0;
-                double time;
-                for(int k = 0; k < 30; k++){
+    cout << "  (ms)   |   Push   |   Pop    |   All 3"    << endl;
+    cout << "---------+----------+----------+----------" << endl;\
 
-                    data << ks[i] << "," << sizes[j] << ",";
+    cout << "Naive    |";
+    cout << fixed << setprecision(4);
+    cout << setw(8) << time_n_pushes_naive(iterations) << " |";
+    cout << setw(8) << time_n_pops_naive(iterations) << "  |";
+    cout << setw(8) << time_multi_naive(iterations) << endl;
 
-                    list = new vector<int>(sizes[j]);
-                    for(int l = 0; l < sizes[j]; l++){
-                        list->at(l) = rand();
-                    }
 
-                    time = time_n_ksorts(*list,ks[i]);
-                    data << time*1000 << "\n";
-                    
-                    //count += time;
-                    delete list;
-                }
-
-                //count = count * 1000 /10;
-                //cout << "Average time for sorting " << sizes[j] << " with " << ks[i] << "-way merge sort is " << count << endl;
-
-            }
-        }
-        /*vector<int>* list;
-        int sz = 27;
-
-        list = new vector<int>(1156);
-        for(int l = 0; l < 1156; l++){
-            list->at(l) = rand();
-        }
-        time_n_ksorts(*list,sz);*/
-        
-        /*cout << "Do you want to continue: ";
-        char check;
-        cin >> check;
-        if(check == 'Y' || check == 'y'){
-            con = true;
-        }
-        else{
-            con = false;
-        }
-        
-
-    }*/
-    data.close();
+    cout << "Improved |";
+    cout << setw(8) << time_n_pushes_improved(iterations) << "  |";
+    cout << setw(8) << time_n_pops_improved(iterations) << "  |";
+    cout << setw(8) << time_multi_improved(iterations) << endl;
 
     return 0;
 }
 
-double time_n_pushes(unsigned n) {
-    stack<unsigned> s;
+double time_n_pushes_improved(unsigned n) {
+    ImprovedStack<unsigned> s;
 
     // get starting clock value
     auto start_time = chrono::system_clock::now();
@@ -95,22 +53,119 @@ double time_n_pushes(unsigned n) {
     // get ending clock value
     auto stop_time = chrono::system_clock::now();
 
-    // compute elapsed time in seconds
-    chrono::duration<double> elapsed = stop_time - start_time;
-    return elapsed.count();
+    // compute elapsed time in microseconds
+    auto elapsed = chrono::duration_cast<chrono::microseconds>(stop_time - start_time);
+    return elapsed.count() / 1000.0;
 }
 
-// double time_n_ksorts(vector<int> nums, int k) {
+double time_n_pushes_naive(unsigned n){
+    NaiveStack<unsigned> s;
 
-//     // get starting clock value
-//     auto start_time = chrono::system_clock::now();
-//     // do the sort
-//     // sorter(nums,k);
-//     nums.at(0) += k;
-//     // get ending clock value
-//     auto stop_time = chrono::system_clock::now();
+    // get starting clock value
+    auto start_time = chrono::system_clock::now();
+    // do the n pushes
+    for (unsigned i = 0; i < n; i++) {
+        s.push(i);
+    }
+    // get ending clock value
+    auto stop_time = chrono::system_clock::now();
 
-//     // compute elapsed time in seconds
-//     chrono::duration<double> elapsed = stop_time - start_time;
-//     return elapsed.count();
-// }
+    // compute elapsed time in microseconds
+    auto elapsed = chrono::duration_cast<chrono::microseconds>(stop_time - start_time);
+    return elapsed.count() / 1000.0;
+}
+
+
+double time_n_pops_improved(unsigned n){
+    ImprovedStack<unsigned> s;
+
+    // do n pushes
+    for (unsigned i = 0; i < n; i++) {
+        s.push(i);
+    }
+    
+    // get starting clock value
+    auto start_time = chrono::system_clock::now();
+
+    // do n pops
+    for (unsigned i = 0; i < n; i++) {
+        s.pop();
+    }
+
+    // get ending clock value
+    auto stop_time = chrono::system_clock::now();
+
+    // compute elapsed time in microseconds
+    auto elapsed = chrono::duration_cast<chrono::microseconds>(stop_time - start_time);
+    return elapsed.count() / 1000.0;
+}
+double time_n_pops_naive(unsigned n){
+    NaiveStack<unsigned> s;
+
+    // do n pushes
+    for (unsigned i = 0; i < n; i++) {
+        s.push(i);
+    }
+    
+    // get starting clock value
+    auto start_time = chrono::system_clock::now();
+
+    // do n pops
+    for (unsigned i = 0; i < n; i++) {
+        s.pop();
+    }
+
+    // get ending clock value
+    auto stop_time = chrono::system_clock::now();
+
+    // compute elapsed time in microseconds
+    auto elapsed = chrono::duration_cast<chrono::microseconds>(stop_time - start_time);
+    return elapsed.count() / 1000.0;
+}
+
+double time_multi_improved(unsigned n){
+    ImprovedStack<unsigned> s;
+    unsigned temp = 0;
+
+    // get starting clock value
+    auto start_time = chrono::system_clock::now();
+    // do n pushes
+    for (unsigned i = 0; i < n; i++) {
+        s.push(i);
+    }
+    // get the top element and pop it
+    for (unsigned i = 0; i < n; i++) {
+        temp = s.top();
+        s.pop();
+    }
+
+    // get ending clock value
+    auto stop_time = chrono::system_clock::now();
+
+    // compute elapsed time in microseconds
+    auto elapsed = chrono::duration_cast<chrono::microseconds>(stop_time - start_time);
+    return elapsed.count() / 1000.0;
+}
+double time_multi_naive(unsigned n){
+    NaiveStack<unsigned> s;
+    unsigned temp = 0;
+
+    // get starting clock value
+    auto start_time = chrono::system_clock::now();
+    // do n pushes
+    for (unsigned i = 0; i < n; i++) {
+        s.push(i);
+    }
+    // get the top element and pop it
+    for (unsigned i = 0; i < n; i++) {
+        temp = s.top();
+        s.pop();
+    }
+
+    // get ending clock value
+    auto stop_time = chrono::system_clock::now();
+
+    // compute elapsed time in microseconds
+    auto elapsed = chrono::duration_cast<chrono::microseconds>(stop_time - start_time);
+    return elapsed.count() / 1000.0;
+}
